70b39f80fa35668e0c88b24fa45add5d09017b5d
4m4r199707 <4m4rarts@gmail.com>
2021-07-23 16:50:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const { MONGOURI } = require('./keys')
+
+mongoose.connect(MONGOURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+mongoose.connection.on("connected", () => {
+    console.log("connected to mongo yeah")
+})
+
+mongoose.connection.on('error', () => {
+    console.log("err connecting", err)
+})
+app.listen(PORT, () => {
+    console.log("server is running on ", PORT)
+})

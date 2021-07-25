da9265f99ef47f21249fc9ed5fdf833e40599339
4m4r199707 <4m4rarts@gmail.com>
2021-07-26 01:03:06 +0530

    user post view route

--- routes/post.js
+++ routes/post.js
@@ -26,5 +26,25 @@ router.post('/createpost',requireLogin,(req,res) => {
     })
 })
 
+router.get('/allpost',(req,res) => {
+    Post.find()
+        .populate("postedBy","_id name")
+        .then( posts => {
+            res.json({posts})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
+router.get('/mypost',requireLogin,(req,res) => {
+    Post.find({postedBy:req.user._id})
+    .populate("PostedBy","_id name")
+    .then(mypost => {
+        res.json({mypost})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
 module.exports = router
